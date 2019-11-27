137469e4ae99dfcef13f48079847c5b4cebfe8fc
Wunkolo <Wunkolo@gmail.com>
2019-11-27 14:11:46 -0800

    Implement basic file CRCing

--- source/main.cpp
+++ source/main.cpp
@@ -5,9 +5,12 @@
 #include <cstring>
 #include <charconv>
 #include <algorithm>
+#include <numeric>
 
 #include <vector>
 #include <filesystem>
+#include <fstream>
+#include <iterator>
 
 #include <getopt.h>
 
@@ -30,6 +33,51 @@ const static struct option CommandOptions[5] = {
 	{ "help",           optional_argument, nullptr,  'h' },
 	{ nullptr,                no_argument, nullptr, '\0' }
 };
+
+std::array<std::uint32_t, 256> generate_crc_lookup_table() noexcept
+{
+	const std::uint32_t reversed_polynomial = 0xEDB88320u;
+
+	// This is a function object that calculates the checksum for a value,
+	// then increments the value, starting from zero.
+	struct byte_checksum
+	{
+		std::uint32_t operator()() noexcept
+		{
+			std::uint32_t CurChecksum = n++;
+
+			for (std::size_t i = 0; i < 8; ++i)
+			{
+				CurChecksum = (CurChecksum >> 1) ^ ((CurChecksum & 0x1u) ? reversed_polynomial : 0);
+			}
+
+			return CurChecksum;
+		}
+		std::uint32_t n = 0;
+	};
+
+	std::array<std::uint32_t, 256> table = {};
+	std::generate(table.begin(), table.end(), byte_checksum{});
+
+	return table;
+}
+ 
+// Calculates the CRC for any sequence of values. (You could use type traits and a
+// static assert to ensure the values can be converted to 8 bits.)
+template <typename InputIterator>
+std::uint32_t crc(InputIterator first, InputIterator last)
+{
+  // Generate lookup table only on first use then cache it - this is thread-safe.
+  static auto const table = generate_crc_lookup_table();
+ 
+  // Calculate the CurChecksum - make sure to clip to 32 bits, for systems that don't
+  // have a true (fast) 32-bit type.
+  return std::uint32_t{0xFFFFFFFFuL} &
+    ~std::accumulate(first, last,
+      ~std::uint32_t{0} & std::uint32_t{0xFFFFFFFFuL},
+        [](std::uint32_t CurChecksum, std::uint8_t CurValue) 
+          { return table[static_cast<std::uint8_t>(CurChecksum ^ CurValue)] ^ (CurChecksum >> 8); });
+}
 
 int main( int argc, char* argv[] )
 {
@@ -86,16 +134,28 @@ int main( int argc, char* argv[] )
 		const std::filesystem::path CurPath(argv[i]);
 		if( !std::filesystem::exists(CurPath) )
 		{
-			std::fprintf(
-				stderr,
-				"Unable to open file: %s\n",
-				argv[i]
-			);
+			std::fprintf(stderr, "File does not exist: %s\n", argv[i]);
+			continue;
 		}
+
+		std::error_code CurError;
+		if( std::filesystem::is_regular_file(CurPath, CurError) )
+		{
+			// Regular file sitting on some storage media, unchanging
+			// Use a faster mmap path.
+		}
+		std::fputs(CurPath.c_str(), stdout);
+
+		CurSettings.InputFiles.emplace_back(CurPath);
+
+		std::ifstream CurFile(CurPath, std::ios::binary);
+		const std::uint32_t CRC32 = crc(
+			std::istream_iterator<char>(CurFile),
+			std::istream_iterator<char>()
+		);
 		std::printf(
-			"%s : %s\n",
-			CurPath.c_str(),
-			 ? "Exists":"Doesn't exist"
+			" %X\n",
+			CRC32
 		);
 		// CurSettings.InputFile = fopen(argv[optind],"rb");
 		// if( CurSettings.InputFile == nullptr )
