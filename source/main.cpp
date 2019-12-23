a8ebe72b8bdebb33782a4d5d992430de5a1b18b7
Wunkolo <Wunkolo@gmail.com>
2019-12-23 14:02:50 -0800

    Add checksum file input, limit file type to regular files

--- source/main.cpp
+++ source/main.cpp
@@ -37,9 +37,10 @@ const char* Usage =
 "  -h, --help               Show this help message\n"
 "  -t, --threads            Number of checker threads in parallel. Default: 2\n";
 
-const static struct option CommandOptions[5] = {
+const static struct option CommandOptions[] = {
 	{ "threads",  optional_argument, nullptr,  't' },
 	{ "help",     optional_argument, nullptr,  'h' },
+	{ "check",    optional_argument, nullptr,  'c' },
 	{ nullptr,          no_argument, nullptr, '\0' }
 };
 
@@ -94,39 +95,32 @@ std::uint32_t Checksum(RandomIterator First, RandomIterator Last, std::random_ac
 	static constexpr auto Table = CRC32Table(Polynomial);
 	std::uint32_t CRC = ~0;
 	const std::size_t Size = static_cast<std::size_t>(std::distance(First, Last));
-
 	const std::uint32_t* Input32 = reinterpret_cast<const std::uint32_t*>(&(*First));
 	std::size_t i;
 
 	// Slice by 8
 #ifdef __AVX2__
+	const __m256i ByteIndex = _mm256_set_epi8(
+		~0, ~0, ~0, 7, ~0, ~0, ~0, 6, ~0, ~0, ~0, 5, ~0, ~0, ~0, 4,
+		~0, ~0, ~0, 3, ~0, ~0, ~0, 2, ~0, ~0, ~0, 1, ~0, ~0, ~0, 0
+	);
+	const __m256i ArrayOffset = _mm256_set_epi32(
+		(sizeof(typename decltype(Table)::value_type) / 4) * 0,
+		(sizeof(typename decltype(Table)::value_type) / 4) * 1,
+		(sizeof(typename decltype(Table)::value_type) / 4) * 2,
+		(sizeof(typename decltype(Table)::value_type) / 4) * 3,
+		(sizeof(typename decltype(Table)::value_type) / 4) * 4,
+		(sizeof(typename decltype(Table)::value_type) / 4) * 5,
+		(sizeof(typename decltype(Table)::value_type) / 4) * 6,
+		(sizeof(typename decltype(Table)::value_type) / 4) * 7 
+	);
 	for( i = 0; i < Size / 8; ++i )
 	{
 		const std::uint64_t Input64 =
 			*reinterpret_cast<const std::uint64_t*>(Input32) ^ CRC;
 		Input32 += 2;
 		const __m256i Indices = _mm256_shuffle_epi8(
-			_mm256_set1_epi64x(Input64),
-			_mm256_set_epi8(
-				~0, ~0, ~0, 7,
-				~0, ~0, ~0, 6,
-				~0, ~0, ~0, 5,
-				~0, ~0, ~0, 4,
-				~0, ~0, ~0, 3,
-				~0, ~0, ~0, 2,
-				~0, ~0, ~0, 1,
-				~0, ~0, ~0, 0
-			)
-		);
-		const __m256i ArrayOffset = _mm256_set_epi32(
-			(sizeof(typename decltype(Table)::value_type) / 4) * 0,
-			(sizeof(typename decltype(Table)::value_type) / 4) * 1,
-			(sizeof(typename decltype(Table)::value_type) / 4) * 2,
-			(sizeof(typename decltype(Table)::value_type) / 4) * 3,
-			(sizeof(typename decltype(Table)::value_type) / 4) * 4,
-			(sizeof(typename decltype(Table)::value_type) / 4) * 5,
-			(sizeof(typename decltype(Table)::value_type) / 4) * 6,
-			(sizeof(typename decltype(Table)::value_type) / 4) * 7 
+			_mm256_set1_epi64x(Input64), ByteIndex
 		);
 		const __m256i Gather = _mm256_i32gather_epi32(
 			reinterpret_cast<const std::int32_t*>(Table.data()),
@@ -189,7 +183,7 @@ int main( int argc, char* argv[] )
 	int Opt;
 	int OptionIndex;
 	// Parse Arguments
-	while( (Opt = getopt_long(argc, argv, "t:h", CommandOptions, &OptionIndex )) != -1 )
+	while( (Opt = getopt_long(argc, argv, "c:t:h", CommandOptions, &OptionIndex )) != -1 )
 	{
 		switch( Opt )
 		{
@@ -197,19 +191,11 @@ int main( int argc, char* argv[] )
 		{
 			std::size_t Threads;
 			const auto ParseResult = std::from_chars<std::size_t>(
-				optarg, optarg + std::strlen(optarg),
-				Threads
+				optarg, optarg + std::strlen(optarg), Threads
 			);
-			if(
-				*ParseResult.ptr != '\0'
-				|| ParseResult.ec != std::errc()
-			)
+			if( *ParseResult.ptr != '\0' || ParseResult.ec != std::errc() )
 			{
-				std::fprintf(
-					stdout,
-					"Invalid thread count \"%s\"\n",
-					optarg
-				);
+				std::fprintf(stdout, "Invalid thread count \"%s\"\n", optarg);
 				return EXIT_FAILURE;
 			}
 			CurSettings.Threads = static_cast<std::size_t>(Threads);
@@ -219,6 +205,11 @@ int main( int argc, char* argv[] )
 		{
 			std::puts(Usage);
 			return EXIT_SUCCESS;
+		}
+		case 'c':
+		{
+			CurSettings.ChecksumFile = std::filesystem::path(optarg);
+			break;
 		}
 		default:
 		{
@@ -248,32 +239,28 @@ int main( int argc, char* argv[] )
 		}
 		std::error_code CurError;
 		std::size_t FileSize = 0;
+		// Regular files only, for now, other files will be specially handled later
 		if( std::filesystem::is_regular_file(CurPath, CurError) )
 		{
 			FileSize = std::filesystem::file_size(CurPath);
+			CurSettings.InputFiles.emplace_back(CurPath);
+			std::fprintf(stdout, "; %zu %s\n", FileSize, CurPath.filename().c_str());
 		}
-		CurSettings.InputFiles.emplace_back(CurPath);
-		std::fprintf(
-			stdout, "; %zu %s\n",
-			FileSize,
-			CurPath.filename().c_str()
-		);
 	}
 
 	for( const auto& CurPath : CurSettings.InputFiles )
 	{
 		std::uint32_t CRC32 = 0;
 		std::error_code CurError;
+		// Regular file sitting on some storage media, unchanging
+		// Use a faster mmap path.
 		if( std::filesystem::is_regular_file(CurPath, CurError) )
 		{
-			// Regular file sitting on some storage media, unchanging
-			// Use a faster mmap path.
-			const auto FileSize = std::filesystem::file_size(CurPath);
+			const std::uintmax_t FileSize = std::filesystem::file_size(CurPath);
 			const auto FileHandle = open(CurPath.c_str(), O_RDONLY, 0);
 			void* FileMap = mmap(
 				nullptr, FileSize,
-				PROT_READ, MAP_SHARED | MAP_POPULATE,
-				FileHandle, 0
+				PROT_READ, MAP_SHARED | MAP_POPULATE, FileHandle, 0
 			);
 			madvise(FileMap, FileSize, MADV_SEQUENTIAL | MADV_WILLNEED);
 			CRC32 = Checksum<0xEDB88320u, const std::uint8_t*>(
@@ -291,12 +278,7 @@ int main( int argc, char* argv[] )
 				std::istreambuf_iterator<char>()
 			);
 		}
-		std::fprintf(
-			stdout, "%s %08X\n",
-			CurPath.filename().c_str(),
-			CRC32
-		);
+		std::fprintf(stdout, "%s %08X\n", CurPath.filename().c_str(), CRC32);
 	}
-
 	return EXIT_SUCCESS;
 }
