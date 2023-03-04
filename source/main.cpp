def915ad320c037b6c4a94dbe13598a8198f22f3
Volker Gropp <volker@gropp.org>
2023-03-04 13:36:45 +0100

    change exit value to atomic  for passed checks

--- source/main.cpp
+++ source/main.cpp
@@ -330,12 +330,12 @@ int Check(const Settings& CurSettings)
 	}
 
 	std::vector<std::thread> Workers;
-	int ExitCode { EXIT_SUCCESS };
+	std::atomic<std::size_t> Passed{0};
 
 	for( std::size_t i = 0; i < CurSettings.Threads; ++i )
 	{
 		Workers.push_back(std::thread(
-			[&ExitCode, &QueueLock,
+			[&Passed, &QueueLock,
 			 &Checkqueue = std::as_const(Checkqueue)](std::size_t WorkerIndex) {
 #ifdef _POSIX_VERSION
 				char ThreadName[16] = {0};
@@ -366,8 +366,8 @@ int Check(const Settings& CurSettings)
 							CurEntry.FilePath.c_str(), CurEntry.Checksum,
 							Valid ? "\e[32m" : "\e[31m", CurSum.value(),
 							Valid ? "\e[32mOK" : "\e[31mFAIL");
-						if( !Valid )
-							ExitCode = EXIT_FAILURE;
+						if( Valid )
+							Passed.fetch_add(1);
 					}
 					else
 					{
@@ -375,7 +375,6 @@ int Check(const Settings& CurSettings)
 							"\e[36m%s\t\e[33m%08X\t\t\e[31mError opening "
 							"file\n",
 							CurEntry.FilePath.c_str(), CurEntry.Checksum);
-						ExitCode = EXIT_FAILURE;
 					}
 				}
 			},
@@ -385,5 +384,5 @@ int Check(const Settings& CurSettings)
 	for( std::size_t i = 0; i < CurSettings.Threads; ++i )
 		Workers[i].join();
 
-	return ExitCode;
+	return Checkqueue.size() == Passed.load() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
